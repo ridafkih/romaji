40c5bd2cae2862bc5d0fe9db61e21f0087a6b225
Rida F'kih <hello@rida.dev>
2021-11-04 10:41:14 -0600

    add toggle component

---
+++ components/Toggle.tsx
@@ -0,0 +1,21 @@
+import React, { Dispatch } from "react";
+
+interface ToggleProps {
+  onLabel: string;
+  offLabel: string;
+  value: boolean;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+const Toggle = ({ onLabel, offLabel, value, onClick }: ToggleProps) => {
+  return (
+    <button
+      onClick={onClick}
+      className="bg-transparent outline-none border-none cursor-pointer hover:underline text-gray-500"
+    >
+      {value ? onLabel : offLabel}
+    </button>
+  );
+};
+
+export default Toggle;
