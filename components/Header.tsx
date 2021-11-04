e7bf71f500fbbeffe3eb325824b92886b32dfe64
Rida F'kih <hello@rida.dev>
2021-11-04 10:40:43 -0600

    add header component

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import next from "next";
+
+const Header = () => {
+  return (
+    <div className="p-8 flex justify-center items-center">
+      <div className="text-4xl text-gray-300 cursor-default select-none">
+        ろまじ
+      </div>
+    </div>
+  );
+};
+
+export default Header;
